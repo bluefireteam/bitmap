7bf59613e03f80526341982e369589f686e8219c
Łukasz Rejman <lukasz.rejman@polidea.com>
2020-02-18 11:07:31 +0100

    Crop Image (#14)
    * Implement crop
    
    * Add documentation comments
    
    * Rename arguments
    
    * Add crop to export list
    
    * Rename args

--- lib/transformations.dart
+++ lib/transformations.dart
@@ -3,5 +3,6 @@ library transformations;
 export 'transformations/adjust_color.dart';
 export 'transformations/brightness.dart';
 export 'transformations/contrast.dart';
+export 'transformations/crop.dart';
 export 'transformations/flip.dart';
 export 'transformations/resize.dart';

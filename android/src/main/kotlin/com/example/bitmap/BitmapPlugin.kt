7970c67823e3f1e3c46cddd64ad872159b4032f4
Pedro Santos <pedro.santos@vizo.dev>
2024-04-14 01:35:04 -0300

    remove unused dependency

--- android/src/main/kotlin/com/example/bitmap/BitmapPlugin.kt
+++ android/src/main/kotlin/com/example/bitmap/BitmapPlugin.kt
@@ -7,7 +7,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** BitmapPlugin */
 class BitmapPlugin: FlutterPlugin, MethodCallHandler {

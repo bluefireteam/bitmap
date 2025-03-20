1a4c6196363ea7106c5a30eb964521db14534509
guptan404 <guptan404@gmail.com>
2025-03-20 19:54:53 +0530

    remove the legacy use of the deprecated Registrar and instead uses the new Flutter embedding API

--- android/src/main/kotlin/com/example/bitmap/BitmapPlugin.kt
+++ android/src/main/kotlin/com/example/bitmap/BitmapPlugin.kt
@@ -1,21 +1,16 @@
 package com.example.bitmap
 
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** BitmapPlugin */
-class BitmapPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class BitmapPlugin : FlutterPlugin, MethodCallHandler {
+
+  private lateinit var channel: MethodChannel
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "bitmap")
@@ -23,10 +18,9 @@ class BitmapPlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      else -> result.notImplemented()
     }
   }
 
